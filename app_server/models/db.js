a7c2088119688b65701ebe785eb408ed33cb3457
danliu277 <danliu277@gmail.com>
2016-10-11 16:44:06 -0400

    Connected Mongoose and created user & house schema

---
+++ app_server/models/db.js
@@ -0,0 +1,66 @@
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://localhost/database';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function(){
+  console.log('Mongoose connected to ',+ dbURI);
+});
+
+mongoose.connection.on('error', function(err){
+  console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function(){
+  console.log('Mongoose disconnected');
+});
+
+gracefulShutdown = function (msg, callback){
+  mongoose.connection.close(function(){
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+//Nodemon restart
+process.once('SUGUSR2', function(){
+  gracefulShutdown('mondemon restart', function(){
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+//App termination
+process.on('SIGINT', function(){
+  gracefulShutdown('app termination', function(){
+    process.exit(0);
+  });
+});
+//Heroku app termination
+process.on('SIGTERM', function(){
+  gracefulShutdown('Heroku app shutdown', function(){
+    process.exit(0);
+  });
+});
+
+var Schema = mongoose.Schema,
+    ObjectID = Schema.ObjectID;
+
+var houseSchema = new Schema({
+  owner     : {type : String, match : /[a-z]/, index: true},
+  address   : {type : String, index : true},
+  zip       : {type : Number, min : 10000, max : 99999,index : true},
+  hood      : {type : String},
+  email     : {type : String, index : true},
+  phone     : {type : String, index : true},
+  features  : {type : [String]},
+  listDate  : {type : Date , index: true},
+  price     : {type : Number, min: 1, index: true},
+  img       : {type : [Buffer], contentType: String}
+});
+
+var userSchema = new Schema({
+  userName      : {type : String, index: true},
+  password      : {type : String, index: true},
+  houseSelling  : [houseSchema],
+  houseBought   : [houseSchema],
+  houseSold     : [houseSchema]
+});
