35de58424f90e14d51c4b8492c23a563616f5cd7
pidgeonhole <pidgeonhole@gitlab.com>
2016-10-19 19:46:23 -0400

    Added geolocation function to index.ejs & printed to log.

--- public/geolocation.js
+++ public/geolocation.js
@@ -1,7 +1,6 @@
-<!-- Geolcation -->
 function geolocation(){
-  //  var x = document.getElementById("demo");
-  //  var y = document.getElementById("ip");
+   var x = document.getElementById("demo");
+   var y = document.getElementById("ip");
    var ip;
 
    // Find location with geolocation
@@ -9,6 +8,9 @@ function geolocation(){
     //  x.innerHTML = "Geolocation: " +
     //  "<br>Latitude: " + position.coords.latitude +
     //  "<br>Longitude: " + position.coords.longitude;
+
+     console.log("Latitude : %s", position.coords.latitude);
+     console.log("Longitude : %s", position.coords.longitude);
    },
    // If user denies access to geolocation use ip address instead
    function (error) {
@@ -18,7 +20,9 @@ function geolocation(){
          $.get("http://ipinfo.io/" + ip, function(responce) {
           //  y.innerHTML = "Ip adress: " + ip +
           //  "<br>Latitude: " + response.loc.split(',')[0] +
-          //  "<br>Longitude: " + response.loc.split(',')[1];
+          //  "<br>Longitude: " + response.loc.split(',')[1];            
+        console.log("Latitude : %s", response.loc.split(',')[0]);
+        console.log("Longitude : %s", response.loc.split(',')[1]);
          }, "jsonp");
      }, "jsonp");
    });
