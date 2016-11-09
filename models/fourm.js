6500fe14979c94350d2483f558d07f824c6e2c78
alan <alancao101@gmail.com>
2016-11-09 18:36:11 -0500

    added schema for user post/comment

---
+++ models/fourm.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    User = require('./user');
+
+module.exports = function(mongoose) {
+    var Post = new Schema({
+        title: String,
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        imageURL: String,
+        content: {
+            type: String,
+            required: true
+        },
+        comments: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }]
+    }, {
+        timestamps: true
+    });
+
+    var Comment = new Schema({
+        content: {
+            type: String,
+            required: true
+        },
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+    }, {
+        timestamps: true
+    });
+
+    // declare seat covers here too
+    var models = {
+        Post: mongoose.model('Post', Post),
+        Comment: mongoose.model('Comment', Comment)
+    };
+
+    return models;
+};
--- router.js
+++ router.js
@@ -1,17 +1,22 @@
 "use strict";
 const express = require('express'),
+    mongoose = require('mongoose'),
     path = require('path'),
     router = express.Router(),
     passport = require('passport'),
     Authentication = require('./middleware/authentication'),
-    passportService = require('./config/passport');
+    passportService = require('./config/passport'),
+    fourm = require('./models/fourm')(mongoose);
 
 router.use(express.static(path.join(__dirname, 'client')));
-// router.use(express.static(path.join(__dirname, 'views')));
-router.use(express.static(path.join(__dirname, 'front_end')));
+router.use(express.static(path.join(__dirname, 'views')));
 
-const requireAuth = passport.authenticate('jwt', {session: false});
-const requireLogin = passport.authenticate('local', {session: false});
+const requireAuth = passport.authenticate('jwt', {
+    session: false
+});
+const requireLogin = passport.authenticate('local', {
+    session: false
+});
 
 // INDEX ROUTE
 router.get('/', function(req, res) {
@@ -24,8 +29,8 @@ router.post('/register', Authentication.register);
 // Login route
 router.post('/login', requireLogin, Authentication.login);
 
-////////////////////WEATHER
-router.get('/user', requireAuth, (req, res) => {
+////////////////////
+router.get('/user', requireAuth, function(req, res) {
     let user = {
         email: req.user.email,
         fullname: req.user.fullname
@@ -33,8 +38,68 @@ router.get('/user', requireAuth, (req, res) => {
     res.json(user);
 });
 
+//////////////////////
+router.post('/post', requireAuth, function(req, res) {
+    const post = new fourm.Post({
+        title: req.body.title,
+        createdBy: req.user._id,
+        imageURL: req.body.imageURL,
+        content: req.body.content
+    });
+
+    post.save(function(err, post) {
+        if (err) {
+            return err;
+        }
+        console.log(post);
+        return res.status(201).send({message: "Create Post Success"});
+    });
+});
+
+router.post('/post/:id', requireAuth, function(req, res) {
+    fourm.Post.findById(req.params.id, function(err, user) {
+        if (err) return err;
+
+        const comment = new fourm.Comment({
+            content: req.body.content,
+            createdBy: req.user._id
+        });
+
+        comment.save(function(err, post) {
+            if (err) {
+                return err;
+            }
+            user.save(
+                user.comments.push(comment._id)
+            );
+            return res.status(201).send({message: "Comment Post Success"});
+        });
+
+    });
+});
+
+router.get('/post', requireAuth, function(req, res) {
+    const Post = fourm.Post;
+    Post
+        .find()
+        .populate('createdBy', 'email fullname')
+        .populate('comments')
+        // .populate({
+        //     path: 'comments',
+        //     populate: {
+        //         path: 'createdBy',
+        //         select: 'email fullname'
+        //     }
+        // })
+        .exec(function(err, Post) {
+            if (err) return res.send(err);
+
+            res.json(Post);
+        });
+});
+
 router.use('*', function(req, res) {
-    let index = path.resolve(__dirname, 'front_end/index.html');
+    let index = path.resolve(__dirname, 'views/index.html');
     res.sendFile(index);
 });
 
