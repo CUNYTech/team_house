a444ae98883c6f04af87dbe776bb72f1a70d92e4
adrian <adrian4k9@gmail.com>
2016-11-16 13:20:29 -0500

    Login works now in angularJS

--- front_end/controller.js
+++ front_end/controller.js
@@ -2,13 +2,21 @@
 //var app = angular.module('homeFinder', ["ngRoute", $scope, $http]);
 var app = angular.module('homeFinder', ["ngRoute"]);
 
-app.controller('mainBody', ['$scope', '$http', function($scope, $http){
-    //$scope.what = "Home Finder";
-$scope.registerUser = function () {
-    console.log($scope.user);
-     $http.post('/register', $scope.user)   
-};
+app.controller('signIn', ['$scope', '$http', function($scope, $http){
+    $scope.logInUser = function () {
+        console.log($scope.user);
+        //$http.post('/login', $scope.user).then(successCallBack, errorCallback);   
+        $http.post('/login', $scope.user)
+            .success(console.log("success"))
+            .error(console.log("error"));
+    };
+}]);
 
+app.controller('signUp', ['$scope', '$http', function($scope, $http){
+    $scope.registerUser = function () {
+        console.log($scope.user);
+        $http.post('/register', $scope.user);   
+    };
 }]);
 
 // app.controller('registerModal', function($scope){
