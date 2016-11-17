1326a0ebb9e3c15c6e941d41d7d5457b734a558c
pidgeonhole <pidgeonhole@gitlab.com>
2016-11-16 19:44:26 -0500

    Some incremental changes in home.js

--- front_end/home/home.js
+++ front_end/home/home.js
@@ -4,15 +4,21 @@ var app = angular.module('homeFinder', ["ngRoute"]);
 
 app.controller('signIn', ['$scope', '$http', function($scope, $http){
     $scope.logInUser = function () {
-        console.log($scope.user);
+        //console.log($scope.user);
         //$http.post('/login', $scope.user).then(successCallBack, errorCallback);   
         $http.post('/login', $scope.user)
             .success(function(response){
 
-            }).error(function(error){
+            })
+            .error(function(error){
                 console.log(error);
             });
     };
+
+    $http.get('/user').then(function(response){
+        $scope.userInfo = response.data;
+        console.log(response.data);
+    });
 }]);
 
 app.controller('signUp', ['$scope', '$http', function($scope, $http){
