dbff0ef91d32926997d974df62afd1487a033b89
alan <alancao101@gmail.com>
2016-11-21 20:52:46 -0500

    test

--- front_end/app.js
+++ front_end/app.js
@@ -1,1 +1,29 @@
 
+(function() {
+    'user strict';
+
+    angular
+        .module('homeFinder', ['ngRoute'])
+        .config(config)
+        .run(run);
+
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'component/home/home.html'
+            })
+            .when('/register', {
+                templateUrl: 'component/register/register.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+        $locationProvider.html5Mode(true);
+    }
+
+    function run($http, $window) {
+        if ($window.localStorage['token']) {
+             $http.defaults.headers.common['Authorization'] = 'JWT ' + $window.localStorage['token'];
+        }
+    }
+})();
