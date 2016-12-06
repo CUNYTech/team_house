0e4c0927c5808b05b5c1517f62e02328bf007228
adrian <adrian4k9@gmail.com>
2016-12-06 11:15:41 -0500

    You can now view posts without being logged in

--- front_end/component/comment/comment.js
+++ front_end/component/comment/comment.js
@@ -3,7 +3,7 @@
 
     angular
         .module('homeFinder')
-        .controller('CommentCtrl', ['$scope', '$http', 'userPost', 'authToken', "$routeParams", '$route', function($scope, $http, userPost,authToken, $routeParams, $route){ 
+        .controller('CommentCtrl', ['$scope', '$http', 'userPost', 'publicPost','authToken', "$routeParams", '$route', function($scope, $http, userPost, publicPost, authToken, $routeParams, $route){ 
             let vm = this;
 
             vm.login = authToken.getToken();
@@ -22,6 +22,25 @@
                                 });
                                 // $location.path('/');
             // $route.reload();
+                vm.submit = function() {
+                    $http.post('/post/' + $routeParams.id, vm.post)
+                        .success(function(response) {
+                            $route.reload();
+                        })
+                        .error(function(error){
+                            console.log(error.error);
+                        });
+                };
+            }
+            else{
+                vm.getInfo = publicPost.commentInfo($routeParams.id)
+                                .then(function(result){
+                                    vm.haveInfo = true;
+                                    $scope.commentInfo = result;
+                                    // console.log($scope.commentInfo);                                  
+                                });
+                                // $location.path('/');
+                 // $route.reload();
                 vm.submit = function() {
                     $http.post('/post/' + $routeParams.id, vm.post)
                         .success(function(response) {
