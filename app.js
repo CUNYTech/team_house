554aedcfb9a20f2c356ab4bcd4402ef36a5363bf
alan <alancao101@gmail.com>
2016-12-05 20:16:25 -0500

    test

--- app.js
+++ app.js
@@ -10,11 +10,10 @@ const express = require('express'),
 // Database Setup
 
 try{
-    var config = require('./config/main');
-    mongoose.connect('mongodb://localhost:27017/house2');
+    var config = process.env || require('./config/main');
+    // mongoose.connect('mongodb://localhost:27017/house2');
 }
 catch(e){
-    config = process.env;
     console.log(e);
 }
 
