b6bf6b161d6b303d8fac9e065dba321206c48ae4
danliu277 <danliu277@gmail.com>
2016-12-06 18:24:13 -0500

    Get zip code from long and lat

--- front_end/service/geolocation.js
+++ front_end/service/geolocation.js
@@ -2,15 +2,28 @@ function geolocation(){
    var x = document.getElementById("demo");
    var y = document.getElementById("ip");
    var ip;
+   var geocoder = new google.maps.Geocoder;
 
    // Find location with geolocation
    navigator.geolocation.getCurrentPosition(function(position) {
-    //  x.innerHTML = "Geolocation: " +
-    //  "<br>Latitude: " + position.coords.latitude +
-    //  "<br>Longitude: " + position.coords.longitude;
-
      console.log("Latitude : %s", position.coords.latitude);
      console.log("Longitude : %s", position.coords.longitude);
+     var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+        if(status == google.maps.GeocoderStatus.OK) {
+            if(results[0]) {
+              let postalCode = results[0].address_components.find(function (component) {
+              return component.types[0] == "postal_code";
+              });
+              console.log(postalCode.long_name);
+            } else {
+                console.log('No results found');
+            }
+        } else {
+            console.log("Error");
+        }
+    });
    },
    // If user denies access to geolocation use ip address instead
    function (error) {
@@ -18,11 +31,25 @@ function geolocation(){
      $.get("http://ipinfo.io", function(response) {
          ip = response.ip;
          $.get("http://ipinfo.io/" + ip, function(responce) {
-          //  y.innerHTML = "Ip adress: " + ip +
-          //  "<br>Latitude: " + response.loc.split(',')[0] +
-          //  "<br>Longitude: " + response.loc.split(',')[1];            
+          //  y.innerHTML = "Ip adress: " + ip +        
         console.log("Latitude : %s", response.loc.split(',')[0]);
         console.log("Longitude : %s", response.loc.split(',')[1]);
+        var latlng = new google.maps.LatLng(response.loc.split(',')[0], response.loc.split(',')[1]);
+
+        geocoder.geocode({'latLng': latlng}, function(results, status) {
+            if(status == google.maps.GeocoderStatus.OK) {
+                if(results[0]) {
+                    let postalCode = results[0].address_components.find(function (component) {
+                    return component.types[0] == "postal_code";
+                });
+                console.log(postalCode.long_name);
+                } else {
+                    console.log('No results found');
+                }
+            } else {
+                console.log("Error");
+            }
+        });
          }, "jsonp");
      }, "jsonp");
    });
