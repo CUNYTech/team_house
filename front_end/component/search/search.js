ba24591a936286ebc2e69f06404bd19c5ddefc94
adrian <adrian4k9@gmail.com>
2016-12-13 14:13:41 -0500

    Search page now has 12 homes, instead of 8

--- front_end/component/search/search.js
+++ front_end/component/search/search.js
@@ -22,7 +22,7 @@
                                     $scope.info = $filter('filter')(result, {zipcode: localStorage.getItem("zipcode")});
                                     localStorage.removeItem("zipcode");
                                     $scope.currentPage = 0;
-                                    $scope.pageSize = 8;
+                                    $scope.pageSize = 12;
                                     $scope.numberOfPages=function(){
                                         return Math.ceil($scope.info.length/$scope.pageSize);                
                                     };
@@ -35,7 +35,7 @@
                                     $scope.info = result;
                                     localStorage.removeItem("zipcode");
                                     $scope.currentPage = 0;
-                                    $scope.pageSize = 8;
+                                    $scope.pageSize = 12;
                                     $scope.numberOfPages=function(){
                                         return Math.ceil($scope.info.length/$scope.pageSize);                
                                     };
@@ -48,7 +48,7 @@
                                     $scope.info = result;
                                     localStorage.removeItem("zipcode");
                                     $scope.currentPage = 0;
-                                    $scope.pageSize = 8;
+                                    $scope.pageSize = 12;
                                     $scope.numberOfPages=function(){
                                         return Math.ceil($scope.info.length/$scope.pageSize);                
                                     };
