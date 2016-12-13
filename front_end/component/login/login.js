a5d28114c664e2f39240f46865acfba2a99fb477
alan <alancao101@gmail.com>
2016-12-12 19:54:34 -0500

    test

--- front_end/component/login/login.js
+++ front_end/component/login/login.js
@@ -3,17 +3,17 @@
 
     angular
         .module('homeFinder')
-        .controller('LoginController', ['$scope', '$http', '$location','authToken', 'toastr', function($scope, $http, $location, authToken, toastr){ 
+        .controller('LoginController', ['$scope', '$http', '$location','authToken', 'toastr', function($scope, $http, $location, authToken, toastr){
             $scope.loginUser = function() {
                 //$http.post('/login', $scope.user).then(successCallBack, errorCallback);
                 $http.post('/login', $scope.user)
-                    .success(function(response) {
+                    .then(function(response) {
                         toastr.success('Login Success');
                         $location.path('/');
                         authToken.setToken(response.token);
                         $http.defaults.headers.common.Authorization = response.token;
                     })
-                    .error(function(error) {
+                    .catch(function(error) {
                         toastr.error("The username and password you entered did not match our records.");
                     });
             };
